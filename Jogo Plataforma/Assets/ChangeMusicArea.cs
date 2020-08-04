5799d6ec749546a3dd97acd17b63016e2a498d20
thegreatgarzet <52185914+thegreatgarzet@users.noreply.github.com>
2020-08-03 21:54:26 -0300

    Update

---
+++ Jogo Plataforma/Assets/ChangeMusicArea.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeMusicArea : MonoBehaviour
+{
+    public int music;
+    public string namearea;
+    public MusicControl musicControl;
+    public AreaName areaName;
+    private void Awake()
+    {
+        musicControl = FindObjectOfType<MusicControl>();
+        areaName = FindObjectOfType<AreaName>();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && areaName.text.text != namearea)
+        {
+            areaName.text.text = namearea;
+            areaName.anim.SetTrigger("changearea");
+            if(musicControl.actualmusic != music)
+            {
+                musicControl.nextmusic = music;
+                musicControl.switchmusic = true;
+            }
+        }
+        
+    }
+}
---
+++ Jogo Plataforma/Assets/Spawn_Ride_Armor.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn_Ride_Armor : MonoBehaviour
+{
+    public GameObject ridearmorPrefab, ridearmorInstance;
+    public bool visible, spawned;
+    public float timer, timerb;
+    private void Update()
+    {
+        if(ridearmorInstance == null && !visible)
+        {
+            //ridearmorInstance = Instantiate(ridearmorPrefab, new Vector2(transform.position.x, transform.position.y + 1.5f), Quaternion.identity);
+        }
+        OnScreen();
+    }
+    public void OnScreen()
+    {
+        Vector3 visTest = Camera.main.WorldToViewportPoint(transform.position);
+        if (visTest.x >= 0 && visTest.x <= 1.3f && visTest.y >= 0 && visTest.y <= 1.3f)
+        {
+            visible = true;
+        }
+        else
+        {
+            visible = false;
+        }
+        if (!visible && !spawned && ridearmorInstance == null)
+        {
+            if (timer > 0)
+            {
+                timer -= Time.deltaTime;
+            }
+            else
+            {
+                InstantiateRide();
+                timer = timerb;
+                spawned = true;
+            }
+        }
+    }
+    public void InstantiateRide()
+    {
+        ridearmorInstance = Instantiate(ridearmorPrefab, new Vector2(transform.position.x, transform.position.y + 1.5f), Quaternion.identity);
+    }
+}
